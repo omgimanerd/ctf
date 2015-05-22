32cfb5db244de813c1cf4d6079b4400233f49f0d
Kenneth Li <noobbyte@gmail.com>
2015-05-22 19:33:58 -0400

    buncha hsctf

--- HSCTF2/window.py
+++ HSCTF2/window.py
@@ -4,19 +4,25 @@ from decimal import *
 import sys
 import time
 
+def genPell(n):
+  pell = [0, 1]
+  if n in [1, 2]:
+    return pell[:(n - 1)]
+  for i in range(2, n):
+    pell.append(2 * pell[i - 1] + pell[i - 2])
+  return pell
 
-limit = Decimal(sys.argv[1])
-
-def isInt(n):
-  return int(n) == float(n)
-
-i = Decimal(1)
-sum = 0
-while i <= limit:
-  hypotenuse = (i * i + (i + 1) * (i + 1)).sqrt()
-  if isInt(hypotenuse):
-    sum += i
-    print i, i + 1, hypotenuse, sum
-  i += 1
-  time.sleep(0)
-  
+def genTriples(n):
+  pell = genPell(int((n / 2) ** 0.5))
+  count = 0
+  for i in range(1, len(pell)):
+    pell1 = pell[i]
+    pell2 = pell[i + 1]
+    triple = sorted([pell2 * pell2 - pell1 * pell1, 2 * pell1 * pell2, pell1 * pell1 + pell2 * pell2])
+    print triple
+    if triple[1] >= n:
+      break
+    count += triple[0]
+  print count
+    
+genTriples(9999999997)
