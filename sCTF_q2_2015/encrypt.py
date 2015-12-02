6dedfdee03fdba7ef0684bb331d4bc776ebbe234
Kenneth Li <kennethli.3470@gmail.com>
2015-12-02 09:38:25 -0500

    stuff

--- sCTF_q2_2015/encrypt.py
+++ sCTF_q2_2015/encrypt.py
@@ -2,15 +2,23 @@ import base64
 import random
 import string
 
-flag = open('flag.txt').read()
+def encrypt():
+    flag = open('flag.txt').read()
+    
+    for i in range(80):
+        if random.random() < 0.5:
+            flag = base64.b64encode(flag.encode('utf8')).decode('utf8')
+        else:
+            alphabet = string.ascii_letters + string.digits
+            shift = random.randint(1,len(alphabet))
+            alphabet_shift = alphabet[shift:] + alphabet[:shift]
+            flag = flag.translate(str.maketrans(alphabet, alphabet_shift))
 
-for i in range(80):
-    if random.random() < 0.5:
-        flag = base64.b64encode(flag.encode('utf8')).decode('utf8')
-    else:
-        alphabet = string.ascii_letters + string.digits
-        shift = random.randint(1,len(alphabet))
-        alphabet_shift = alphabet[shift:] + alphabet[:shift]
-        flag = flag.translate(str.maketrans(alphabet, alphabet_shift))
+    open('encrypted.txt','w').write(flag)
 
-open('encrypted.txt','w').write(flag)
+def decrypt():
+    enc = open('encrypted.txt', 'r').read()
+    for i in range(80):
+        if :
+
+        
